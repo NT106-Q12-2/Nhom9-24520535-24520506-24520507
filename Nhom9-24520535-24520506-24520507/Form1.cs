b042ef6edce9c00e54822460652f48ce66d6ec2f
LENOVO <LENOVO@LAPTOP-7V3UQBAO>
2025-11-22 14:46:34 +0700

    Add project files.

---
+++ Nhom9-24520535-24520506-24520507/Form1.cs
@@ -0,0 +1,10 @@
+namespace Nhom9_24520535_24520506_24520507
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
